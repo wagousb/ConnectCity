de7839a8c24f3a60267798dbbdb0edc8a26813e8
[dyad] <git@dyad.sh>
2025-10-18 12:18:41 -0300

    [dyad] Corrigindo ícone e aprimorando a moderação - wrote 1 file(s)

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -153,3 +153,13 @@ export const LightbulbIcon = (props: React.SVGProps<SVGSVGElement>) => (
         <path d="M12 2a7 7 0 0 0-7 7c0 3.03 1.09 5.14 2.5 6.58.91.91 1.5 2.32 1.5 3.42V20h6v-1c0-1.1.59-2.51 1.5-3.42C17.91 14.14 19 12.03 19 9a7 7 0 0 0-7-7z" />
     </svg>
 );
+
+export const FileTextIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"/>
+        <polyline points="14 2 14 8 20 8"/>
+        <line x1="16" y1="13" x2="8" y2="13"/>
+        <line x1="16" y1="17" x2="8" y2="17"/>
+        <line x1="10" y1="9" x2="8" y2="9"/>
+    </svg>
+);
