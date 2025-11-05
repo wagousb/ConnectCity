5e8ecba21667684b6acfa0a40c7e936e04d03838
[dyad] <wagou.sb@gmail.com>
2025-11-05 02:43:53 -0300

    [dyad] Corrigindo o corte do menu lateral esquerdo adicionando rolagem interna. - wrote 2 file(s)

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -12,9 +12,11 @@ interface LeftSidebarProps {
 
 const LeftSidebar: React.FC<LeftSidebarProps> = ({ user, currentView, onViewChange, isModerator }) => {
   return (
-    <aside className="space-y-6 sticky top-24">
-      <ProfileCard user={user} onViewChange={(viewName) => onViewChange({ view: viewName })} />
-      <NavLinks activeLink={currentView} onLinkClick={(viewName) => onViewChange({ view: viewName })} isModerator={isModerator} />
+    <aside className="sticky top-24 max-h-[calc(100vh-6rem)] overflow-y-auto">
+      <div className="space-y-6">
+        <ProfileCard user={user} onViewChange={(viewName) => onViewChange({ view: viewName })} />
+        <NavLinks activeLink={currentView} onLinkClick={(viewName) => onViewChange({ view: viewName })} isModerator={isModerator} />
+      </div>
     </aside>
   );
 };
